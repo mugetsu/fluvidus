fd9139b435dded94c11c6e2041dd7344166904a0
Randell Quitain <cprjk.buzz@gmail.com>
2013-07-22 19:07:01 +0800

    minify _adjuster()
    Signed-off-by: Randell Quitain <cprjk.buzz@gmail.com>

--- scripts/fluvidus.dev.js
+++ scripts/fluvidus.dev.js
@@ -17,35 +17,31 @@
             // Initialize correct data
             _adjuster(element, this.options, 0);
 
-            // Run the pot molder
+            // Spin the pot molder
             this.applyFluidity(element, this.options);
             this.applyNavigation(element, this.options);
             this.applyEvents(element, this.options);
         };
 
         // Public function
-        this.test = function (element, options) {
-            console.log('Test');
-        };
-        
         this.applyFluidity = function (element, options) {
             $(options.frame, element).css({ left: -(100) + '%', width: _framing(element, options) + '%' });
             $(options.child, element).width( _orienting(element, options) + '%' );
         };
 
         this.applyNavigation = function (element, options) {
+            var navItem, navType;
             if (options.pager === true) {
-                var nav_item;
                 for (i = 1; i <= options.childItems.length; i++) {
-                    (i === 1) ? nav_item = '<li class="' + options.navItemLabel + '"><a class="fluvidus-button fluvidus-button-active" href="#">' + i + '</a></li>' : nav_item += '<li class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + i + '</a></li>';
+                    (i === 1) ? navItem = '<li class="' + options.navItemLabel + '"><a class="fluvidus-button fluvidus-button-active" href="#">' + i + '</a></li>' : navItem += '<li class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + i + '</a></li>';
                 }
-                $(element).append('<ul class="fluvidus-pager">' + nav_item + '</ul>');
+                navType = 'fluvidus-pager';
             } else {
-                var nav_buttons;
-                nav_buttons = '<li id="' + options.prevId + '" class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + options.navPrevLabel + '</a></li>';
-                nav_buttons += '<li id="' + options.nextId + '" class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + options.navNextLabel + '</a></li>';
-                $(element).append('<ul class="fluvidus-nav">' + nav_buttons + '</ul>');
+                navItem = '<li id="' + options.prevId + '" class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + options.navPrevLabel + '</a></li>';
+                navItem += '<li id="' + options.nextId + '" class="' + options.navItemLabel + '"><a class="fluvidus-button" href="#">' + options.navNextLabel + '</a></li>';
+                navType = 'fluvidus-nav';
             }
+            $(element).append('<ul class="' + navType + '">' + navItem + '</ul>');
         };
 
         this.applyEvents = function (element, options) {
@@ -54,19 +50,13 @@
             $(document.getElementById(options.nextId), element).click(function(e) {
                 e.preventDefault();
                 pointer++;
-                (pointer >= last) ? pointer = 0 : pointer;
-                
-                _adjuster(element, options, pointer);
-                _animator(element, options, 1);
+                _adjuster(element, options, (pointer >= last) ? pointer = 0 : pointer, 1);
             });
 
             $(document.getElementById(options.prevId), element).click(function(e) {
                 e.preventDefault();
                 pointer--;
-                (pointer < 0) ? pointer = (last - 1) : pointer;
-                
-                _adjuster(element, options, pointer);
-                _animator(element, options, -1);
+                _adjuster(element, options, (pointer < 0) ? pointer = (last - 1) : pointer, -1);
             });
         };
 
@@ -87,36 +77,34 @@
         return ($(options.frame, element).width() / ($(options.frame, element).width() * (options.childItems.length - 1))) * 100;
     }
 
-    function _adjuster(element, options, pointer) {
-        var total = (options.childItems.length - 1),
-            curr = pointer,
-            next = ((pointer + 1) > total) ? 0 : pointer + 1,
-            prev = (pointer === 0) ? total : pointer - 1,
-            availImg = $('.fluvidus-item img').length,
-            availP = $('.fluvidus-item p').length;
-
-        console.log('pointer:'+pointer+', curr:'+curr+', next:'+next+', prev:'+prev);
+    function _adjuster(element, options, pointer, direction) {
+        var itemContent = $(options.child, element).children().length,
+            total = (options.childItems.length - 1),
+            points = [
+                { prev: (pointer === 0) ? total : pointer - 1 },
+                { curr: pointer },
+                { next: ((pointer + 1) > total) ? 0 : pointer + 1 }
+            ];
         
-        if((availImg && availP) == 0) {
-            $('#fluvidus-item-prev', element).append('<img src="' + options.childItems[prev].hero + '" alt="' + options.childItems[prev].desc + '"/><p>' + options.childItems[prev].desc + '</p>');
-            $('#fluvidus-item-curr', element).append('<img src="' + options.childItems[curr].hero + '" alt="' + options.childItems[curr].desc + '"/><p>' + options.childItems[curr].desc + '</p>');
-            $('#fluvidus-item-next', element).append('<img src="' + options.childItems[next].hero + '" alt="' + options.childItems[next].desc + '"/><p>' + options.childItems[next].desc + '</p>');        
-        } else {
-            $('#fluvidus-item-prev', element).find('img').prop({
-                'src': options.childItems[prev].hero,
-                'alt': options.childItems[prev].desc
-            }).end().find('p').text(options.childItems[prev].desc);
-
-            $('#fluvidus-item-curr', element).find('img').prop({
-                'src': options.childItems[curr].hero,
-                'alt': options.childItems[curr].desc
-            }).end().find('p').text(options.childItems[curr].desc);
-
-            $('#fluvidus-item-next', element).find('img').prop({
-                'src': options.childItems[next].hero,
-                'alt': options.childItems[next].desc
-            }).end().find('p').text(options.childItems[next].desc);
+        for(var i = 0; i < points.length; i++) {
+            for(var key in points[i] ) {
+                if(itemContent === 0) {
+                    $(document.getElementById(options.frameBase[i].frameId), element).append('<img src="' + options.childItems[points[i][key]].hero + '" alt="' + options.childItems[points[i][key]].desc + '"/><p>' + options.childItems[points[i][key]].desc + '</p>');
+                } else {
+                    for(var i = 0; i < points.length; i++) {
+                        for(var key in points[i] ) {
+                            $(document.getElementById(options.frameBase[i].frameId), element).find('img').prop({
+                                'src': options.childItems[points[i][key]].hero,
+                                'alt': options.childItems[points[i][key]].desc
+                            }).end().find('p').text(options.childItems[points[i][key]].desc);
+                        }
+                    }
+                }
+            }
         }
+
+        _animator(element, options, direction);
+        
     }
 
     function _animator(element, options, direction) {
@@ -135,6 +123,11 @@
 
     $.fluvidus.defaults = {
         frame: '.fluvidus-frame',
+        frameBase: [
+            { frameId: 'fluvidus-item-prev' },
+            { frameId: 'fluvidus-item-curr' },
+            { frameId: 'fluvidus-item-next' }
+        ],
         child: '.fluvidus-item',
         childActiveLabel: 'fluvidus-item-active',
         childItems: [{
